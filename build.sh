9724e321fb12ad362347c0dc63cb1901c0556f6d
Carlos Pascual <>
2024-12-16 19:01:56 +0100

    Output built files in src/gnmi_pb2/

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Define the path to the proto folder
-PROTO_DIR="$(dirname $0)/proto"
+ROOT_DIR="$(dirname $0)"
+PROTO_DIR="$ROOT_DIR/proto"
+SRC_DIR="$ROOT_DIR/src/gnmi_pb2"
 
 # Compile the gNMI proto file to .py and .pyi files
-uv run python -m grpc_tools.protoc -I$PROTO_DIR --python_out=$PROTO_DIR --pyi_out=$PROTO_DIR --grpc_python_out=$PROTO_DIR $PROTO_DIR/gnmi.proto;
+uv run python -m grpc_tools.protoc -I$PROTO_DIR --python_out=$SRC_DIR --pyi_out=$SRC_DIR --grpc_python_out=$SRC_DIR gnmi_ext.proto gnmi.proto;
