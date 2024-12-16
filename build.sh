be6ef8ef56afe6956376a4f51272047c45892628
Carlos Pascual <>
2024-12-16 20:10:00 +0100

    Rename src dir

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # Define the path to the proto folder
 ROOT_DIR="$(dirname $0)"
 PROTO_DIR="$ROOT_DIR/proto"
-SRC_DIR="$ROOT_DIR/src/gnmi_pb2"
+SRC_DIR="$ROOT_DIR/src/gnmi_pb"
 
 # Compile the gNMI proto file to .py and .pyi files
 uv run python -m grpc_tools.protoc -I$PROTO_DIR --python_out=$SRC_DIR --pyi_out=$SRC_DIR --grpc_python_out=$SRC_DIR gnmi_ext.proto gnmi.proto;
